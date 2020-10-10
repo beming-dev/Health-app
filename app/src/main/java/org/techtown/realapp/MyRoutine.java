db90c5a5ed66dd95969be402489c0421183d71a0
progmins <31382341+progmins@users.noreply.github.com>
2020-10-10 15:07:22 +0900

    create myroutine

---
+++ app/src/main/java/org/techtown/realapp/MyRoutine.java
@@ -0,0 +1,13 @@
+package org.techtown.realapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MyRoutine extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.myroutine);
+    }
+}
---
+++ app/src/main/java/org/techtown/realapp/MyRoutineAdapter.java
@@ -0,0 +1,25 @@
+package org.techtown.realapp;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyRoutineAdapter extends RecyclerView.Adapter<MyRoutineAdapter.ViewHolder> {
+
+    @NonNull
+    @Override
+    public MyRoutineAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyRoutineAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
