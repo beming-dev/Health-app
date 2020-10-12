30f794fe0388c6264dfbd4529fe45193a9562bc7
progmins <31382341+progmins@users.noreply.github.com>
2020-10-12 22:53:00 +0900

    set textview when oncreate called at routinenumber

--- app/src/main/java/org/techtown/realapp/RoutineNumber.java
+++ app/src/main/java/org/techtown/realapp/RoutineNumber.java
@@ -22,6 +22,14 @@ import java.util.ArrayList;
 
 public class RoutineNumber extends AppCompatActivity {
     ArrayList<Ex> exercise;
+    ArrayList<Ex> exercise_1;
+    ArrayList<Ex> exercise_2;
+    ArrayList<Ex> exercise_3;
+    ArrayList<Ex> exercise_4;
+    TextView textView_day1;
+    TextView textView_day2;
+    TextView textView_day3;
+    TextView textView_day4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +44,36 @@ public class RoutineNumber extends AppCompatActivity {
         Button btn_day2 = findViewById(R.id.btn_day2);
         Button btn_day3 = findViewById(R.id.btn_day3);
         Button btn_day4 = findViewById(R.id.btn_day4);
+        textView_day1 = findViewById(R.id.textview_day1);
+        textView_day2 = findViewById(R.id.textview_day2);
+        textView_day3 = findViewById(R.id.textview_day3);
+        textView_day4 = findViewById(R.id.textview_day4);
+
+        exercise_1 = ReadExerciseData(Constants.EX_SHP_KEY_day1);
+        exercise_2 = ReadExerciseData(Constants.EX_SHP_KEY_day2);
+        exercise_3 = ReadExerciseData(Constants.EX_SHP_KEY_day3);
+        exercise_4 = ReadExerciseData(Constants.EX_SHP_KEY_day4);
+
+        //set textview when oncreate called
+
+        textView_day1.setText(null);
+        textView_day2.setText(null);
+        textView_day3.setText(null);
+        textView_day4.setText(null);
+        for(int i=0; i<exercise_1.size(); i++) {
+            if(exercise_1.get(i).getChoosed() == 1) {
+                textView_day1.append(exercise_1.get(i).getName() + "\n");
+            }
+            if(exercise_2.get(i).getChoosed() == 1) {
+                textView_day2.append(exercise_2.get(i).getName() + "\n");
+            }
+            if(exercise_3.get(i).getChoosed() == 1) {
+                textView_day3.append(exercise_3.get(i).getName() + "\n");
+            }
+            if(exercise_4.get(i).getChoosed() == 1) {
+                textView_day4.append(exercise_4.get(i).getName() + "\n");
+            }
+        }
 
         //complete -> mainActivity
         Intent intent = getIntent();
@@ -114,10 +152,6 @@ public class RoutineNumber extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        TextView textView_day1 = findViewById(R.id.textview_day1);
-        TextView textView_day2 = findViewById(R.id.textview_day2);
-        TextView textView_day3 = findViewById(R.id.textview_day3);
-        TextView textView_day4 = findViewById(R.id.textview_day4);
         textView_day1.setMovementMethod(new ScrollingMovementMethod());
         textView_day2.setMovementMethod(new ScrollingMovementMethod());
         textView_day3.setMovementMethod(new ScrollingMovementMethod());
