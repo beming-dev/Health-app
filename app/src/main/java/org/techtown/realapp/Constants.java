dfe8b30bc9ce7cb764397b4633a13fd9a1993e8b
progmins <31382341+progmins@users.noreply.github.com>
2020-10-15 00:33:26 +0900

    I need many edit

--- app/src/main/java/org/techtown/realapp/Constants.java
+++ app/src/main/java/org/techtown/realapp/Constants.java
@@ -5,6 +5,7 @@ public final class Constants {
     final public static String EX_SHP_KEY_day2 = "shp_key_2";
     final public static String EX_SHP_KEY_day3 = "shp_key_3";
     final public static String EX_SHP_KEY_day4 = "shp_key_4";
+    final public static String EX_SHP_KEY_todayEx = "todayEx";
 
     final public static String EX_SHP_DATA_KEY = "shp_data_key";
 
@@ -12,6 +13,8 @@ public final class Constants {
     final public static int EX_LOWER_START = 8;
     final public static int EX_DIET_START = 16;
     final public static int EX_CORE_START = 24;
+
+    final public static int EX_COUNT = 24;
 
     final public static int DEFAULT_SET = 3;
     final public static int DEFAULT_NUMBER = 10;
