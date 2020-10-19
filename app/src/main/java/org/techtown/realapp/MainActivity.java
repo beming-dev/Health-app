84f9bdf85ded96183d868c9d27a851db52c1d551
progmins <31382341+progmins@users.noreply.github.com>
2020-10-19 20:24:11 +0900

    Revert "."
    This reverts commit 1b692c6ea60bac167cd7aaee4f8f2158a797c7ae.

--- app/src/main/java/org/techtown/realapp/MainActivity.java
+++ app/src/main/java/org/techtown/realapp/MainActivity.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 
-import com.google.android.gms.ads.MobileAds;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
@@ -32,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
 
         Button btn_today = findViewById(R.id.btn_today);
         Button btn_routine = findViewById(R.id.btn_routine);
